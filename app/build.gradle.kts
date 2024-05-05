a02648fde2d3907ab0b9f8750dbf7112f68ebafc
Evgeniy Mokich <mokichzhenya@gmail.com>
2024-05-05 13:31:46 +0200

    updated versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.myspeechy.myspeechy"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "4.0"
+        versionCode = 5
+        versionName = "5.0"
         buildTypes {
             release {
                 ndk {
